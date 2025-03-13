a8b45ab3f18234dc18f710e0638c9e1b8adc2e54
LuMariGames <kai.hono426@gmail.com>
2025-03-13 14:10:01 +0900

    Update renderqueue.c

--- source/renderqueue.c
+++ source/renderqueue.c
@@ -75,7 +75,7 @@ static void onQueueFinish(gxCmdQueue_s* queue)
 void C3D_FrameSync(void)
 {
 	u64 time = osGetTime();
-	u64 start = time + 0.016666;
+	u64 start = time + 16;
 	do
 	{
 		time = osGetTime();
